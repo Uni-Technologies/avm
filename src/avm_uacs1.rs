09f14a2bf89d243168bc14c8c472f9fbbea11a0f
Ist <97103242+IstsPrograms@users.noreply.github.com>
2024-02-08 14:10:12 +0300

    UACS1 updated

--- src/avm_uacs1.rs
+++ src/avm_uacs1.rs
@@ -33,6 +33,45 @@ pub fn change_fg2(memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> V
     return VMCode::Success;
 }
 
+pub fn clear_scrn(_memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
+    print!("\x1b[2J");
+    std::io::stdout().flush().unwrap();
+    return VMCode::Success;
+}
+
+pub fn move_cursor(memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
+    let direction = match memory.get_by(_args[0] as usize) {
+        0x0 => "A",
+        0x1 => "B",
+        0x2 => "C",
+        0x3 => "D",
+        _ => "A"
+    };
+    let num = memory.get_by(_args[1] as usize);
+    print!("\x1b[{0}{1}", num, direction);
+    std::io::stdout().flush().unwrap();
+    return VMCode::Success;
+}
+
+pub fn set_cursor(memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
+    let a = memory.get_by(_args[0] as usize);
+    let b = memory.get_by(_args[1] as usize);
+    print!("\x1b[{};{}H", a, b);
+    std::io::stdout().flush().unwrap();
+    return VMCode::Success;
+}
+
+pub fn toggle_cursor(memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
+    let enabled = match memory.get_by(_args[0] as usize) {
+        0x0 => "?25l",
+        0x1 => "?25h",
+        _ => "?25h"
+    };
+    print!("\x1b[{}", enabled);
+    std::io::stdout().flush().unwrap();
+    return VMCode::Success;
+}
+
 pub fn get_instructions_for_uacs1() ->Vec<(i32, fn(&mut Memory, Vec<i32>,&mut i32) -> VMCode)> {
     return vec![
         (0x00, add_from),
@@ -59,5 +98,9 @@ pub fn get_instructions_for_uacs1() ->Vec<(i32, fn(&mut Memory, Vec<i32>,&mut i3
         (0x15, change_bg2),
         (0x16, change_fg1),
         (0x17, change_fg2),
+        (0x18, clear_scrn),
+        (0x19, move_cursor),
+        (0x1A, toggle_cursor),
+        (0x1B, set_cursor)
     ];
 }
