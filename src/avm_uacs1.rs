73333d1ae87321de0933d94bedaf6f43b854c8d4
Ist <97103242+IstsPrograms@users.noreply.github.com>
2024-02-08 14:57:32 +0300

    UACS1 another update

--- src/avm_uacs1.rs
+++ src/avm_uacs1.rs
@@ -39,6 +39,12 @@ pub fn clear_scrn(_memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> 
     return VMCode::Success;
 }
 
+pub fn reset_styles(_memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
+    print!("\x1b[0m");
+    std::io::stdout().flush().unwrap();
+    return VMCode::Success;
+}
+
 pub fn move_cursor(memory: &mut Memory, _args: Vec<i32>, _curr_op: &mut i32) -> VMCode {
     let direction = match memory.get_by(_args[0] as usize) {
         0x0 => "A",
@@ -101,6 +107,7 @@ pub fn get_instructions_for_uacs1() ->Vec<(i32, fn(&mut Memory, Vec<i32>,&mut i3
         (0x18, clear_scrn),
         (0x19, move_cursor),
         (0x1A, toggle_cursor),
-        (0x1B, set_cursor)
+        (0x1B, set_cursor),
+        (0x1C, reset_styles)
     ];
 }
